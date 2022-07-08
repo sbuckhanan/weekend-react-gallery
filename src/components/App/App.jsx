a0942625dcc4b76da89f437688b3d72638934ecb
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-08 18:43:37 -0500

    finish moving everything to components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -38,27 +38,7 @@ function App() {
 		<div className='App'>
 			<Header />
 			<Form />
-			<div className='card'>
-				<div className='card__content'>
-					<img src='images/goat_small.jpg' />
-					<p className='description'>Description goes here</p>
-					<button className='likeButton'>LIKE</button>
-				</div>
-			</div>
-			<div className='card'>
-				<div className='card__content'>
-					<img src='images/goat_small.jpg' />
-					<p className='description'>Description goes here</p>
-					<button className='likeButton'>LIKE</button>
-				</div>
-			</div>
-			<div className='card'>
-				<div className='card__content'>
-					<img src='images/goat_small.jpg' />
-					<p className='description'>Description goes here</p>
-					<button className='likeButton'>LIKE</button>
-				</div>
-			</div>
+			<GalleryList />
 		</div>
 	);
 }
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,11 +1,29 @@
 function GalleryList() {
 	return (
-		<div className='card'>
-			<div className='card__content'>
-				<img src='images/goat_small.jpg' />
-				<p className='description'>Description goes here</p>
-				<button className='likeButton'>LIKE</button>
+		<>
+			<div className='card'>
+				<div className='card__content'>
+					<img src='images/goat_small.jpg' />
+					<p className='description'>Description goes here</p>
+					<button className='likeButton'>LIKE</button>
+				</div>
 			</div>
-		</div>
+			<div className='card'>
+				<div className='card__content'>
+					<img src='images/goat_small.jpg' />
+					<p className='description'>Description goes here</p>
+					<button className='likeButton'>LIKE</button>
+				</div>
+			</div>
+			<div className='card'>
+				<div className='card__content'>
+					<img src='images/goat_small.jpg' />
+					<p className='description'>Description goes here</p>
+					<button className='likeButton'>LIKE</button>
+				</div>
+			</div>
+		</>
 	);
 }
+
+export default GalleryList;
