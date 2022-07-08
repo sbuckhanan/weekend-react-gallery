0d724032b1edc2697a7ef8ae75988e500961db4b
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-08 16:44:11 -0500

    add more style and className

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -46,8 +46,22 @@ function App() {
 			<div className='card'>
 				<div className='card__content'>
 					<img src='images/goat_small.jpg' />
-					<p>Gallery goes here</p>
-					<button>LIKE</button>
+					<p className='description'>Description goes here</p>
+					<button className='likeButton'>LIKE</button>
+				</div>
+			</div>
+			<div className='card'>
+				<div className='card__content'>
+					<img src='images/goat_small.jpg' />
+					<p className='description'>Description goes here</p>
+					<button className='likeButton'>LIKE</button>
+				</div>
+			</div>
+			<div className='card'>
+				<div className='card__content'>
+					<img src='images/goat_small.jpg' />
+					<p className='description'>Description goes here</p>
+					<button className='likeButton'>LIKE</button>
 				</div>
 			</div>
 		</div>
